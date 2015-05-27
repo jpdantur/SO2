87807869f4322fac55996b8adcb87131df5ed267
jpdantur <jpdantur@gmail.com>
2015-05-27 18:57:56 -0300

    Add read registers functions

--- Kernel/interrupts.c
+++ Kernel/interrupts.c
@@ -1,10 +1,30 @@
 #include <naiveConsole.h>
 
+#define CALL_WRITE 4
+
 char get_call(void);
+char get_rax(void);
+char get_rcx(void);
 
 void int80()
 {
-	char c = get_call();
-	ncPrintChar(c);	
+	/*char c = get_call();
+	ncPrintChar(c);*/
+
+	char call = get_rax();
+
+	switch (call)
+	{
+		case CALL_WRITE:
+			screen_write();
+			break;
+		default:
+	 		break;
+	}
 }
 
+void screen_write()
+{
+	char c = get_rcx();
+	ncPrintChar(c);
+}
--- Kernel/libasm.asm
+++ Kernel/libasm.asm
@@ -1,5 +1,26 @@
 global get_call
 
+global get_rax
+global get_rbx
+global get_rcx
+global get_rdx
+
 get_call:
 	mov rax, rcx
 	ret
+
+get_rax:
+	ret
+
+get_rbx:
+	mov rax, rbx
+	ret
+
+get_rcx:
+	mov rax, rcx
+	ret
+
+get_rdx:
+	mov rax, rdx
+	ret
+
