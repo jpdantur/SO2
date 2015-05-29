8ba81bc1b83f856af99064ccfb35289589c8ae99
jpdantur <jpdantur@gmail.com>
2015-05-29 18:16:04 -0300

    new_line working

--- Kernel/drivers/keyboard.c
+++ Kernel/drivers/keyboard.c
@@ -3,7 +3,23 @@
 #include <video.h>
 
 void keyboard(){
-	char c=read_port(0x60);
-	video_write_byte('f');
+	unsigned char status;
+	char keycode;
+
+	status = read_port(0x64);
+	/* Lowest bit of status will be set if buffer is not empty */
+	if (status & 0x01) {
+		keycode = read_port(0x60);
+		if(keycode < 0)
+			return;
+		switch(keyboard_map[keycode]){
+			case '\n':
+				video_new_line();
+				break;
+			default:
+				video_write_byte(keyboard_map[keycode]);
+				break;
+		}	
+	}
 	
 }
