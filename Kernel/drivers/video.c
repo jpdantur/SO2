d3b18d351f5fec0f447edc14882124dacf010222
Mister Poronga <jpdantur@gmail.com>
2015-06-05 15:35:45 -0300

    Introducing cursor

--- Kernel/drivers/video.c
+++ Kernel/drivers/video.c
@@ -53,6 +53,8 @@ void video_write_byte(char c)
 {
 	if (c == BACKSPACE_BYTE){
 		video_backspace();
+		*(video)='_';
+		*(video+2)=0;
 		return;
 	}
 
@@ -74,6 +76,7 @@ void video_write_byte(char c)
 	}
 	
 	*video = c;
+	*(video+2)='_';
 	video_next_char();
 	video_bff_counter_inc();
 }
