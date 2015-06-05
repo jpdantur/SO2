70df8c1b37780c5e265e378cf6eca9f9000205bf
Mister Poronga <jpdantur@gmail.com>
2015-06-05 16:07:30 -0300

    Full Shift Keyboard

--- Kernel/drivers/keyboard.c
+++ Kernel/drivers/keyboard.c
@@ -45,6 +45,46 @@ unsigned char keyboard_map[128] =
     0,	/* All other keys are undefined */
 };
 
+unsigned char shift_map[128] =
+{
+    0,  27, '!', '@', '#', '$', '%', '^', '&', '*', /* 9 */
+  '(', ')', '_', '=', '\b', /* Backspace */
+  '\t',     /* Tab */
+  'Q', 'W', 'E', 'R', /* 19 */
+  'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n', /* Enter key */
+    0,      /* 29   - Control */
+  'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', /* 39 */
+ '\"', '~',   0,    /* Left shift */
+ '|', 'Z', 'X', 'C', 'V', 'B', 'N',      /* 49 */
+  'M', '<', '>', '?',   0,        /* Right shift */
+  '*',
+    0,  /* Alt */
+  ' ',  /* Space bar */
+    0,  /* Caps lock */
+    0,  /* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,  /* < ... F10 */
+    0,  /* 69 - Num lock*/
+    0,  /* Scroll Lock */
+    0,  /* Home key */
+    0,  /* Up Arrow */
+    0,  /* Page Up */
+  '-',
+    0,  /* Left Arrow */
+    0,
+    0,  /* Right Arrow */
+  '+',
+    0,  /* 79 - End key*/
+    0,  /* Down Arrow */
+    0,  /* Page Down */
+    0,  /* Insert Key */
+    0,  /* Delete Key */
+    0,   0,   0,
+    0,  /* F11 Key */
+    0,  /* F12 Key */
+    0,  /* All other keys are undefined */
+};
+
 char buffer[256];
 
 unsigned int w = 0;
@@ -95,8 +135,8 @@ void keyboard_buffer_write(){
                     break;
               */
                 default:
-                    if ((shift1 || shift2 || capslock) && is_letter(keyboard_map[keycode]))
-                        buffer[w%256] = keyboard_map[keycode] - ('a'-'A');
+                    if (shift1 || shift2 || capslock)
+                        buffer[w%256] = shift_map[keycode];
                     else
                         buffer[w%256] = keyboard_map[keycode];
             
